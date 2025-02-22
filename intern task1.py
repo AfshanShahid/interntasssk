ac11d1526628f501f2a4c2948ae1e9db8e0cc1fc
AfshanShahid <daargulfam@gmail.com>
2025-02-22 11:28:15 +0500

    Add files via upload

---
+++ intern task1.py
@@ -0,0 +1,122 @@
+#Task 1
+#VARIABLES AND BASIC DATA MANIPULATION
+#part 1
+#Create a Python script that declares the variable in which print All information about yourself.
+name="Afshan Shahid"
+gender="Female"
+age="22"
+email="aaa@gmail.com"
+phone="23465767841"
+address="Sheikhpura"
+degree="BSCS"
+dob="02-11-2002"
+CNIC="35404-11112367-1"
+nationality="Pakistani"
+print(name)
+print(gender)
+print(age)
+print(email)
+print(phone)
+print(address)
+print(degree)
+print(dob)
+print(CNIC)
+print(nationality)
+
+#part 2
+#Write a Python script that demonstrates type casting while swapping two variables. And print the values before and after swapping.
+
+
+a=10
+b=15
+print("Before Swapping")
+print(a)
+print(b)
+a,b=b,a
+print("After Swapping")
+print(a)
+print(b)
+
+#part 3
+#Declared a Variable With different Data Types in Python and display along with their data types.
+int=10
+str="Afshan"
+float=10.5
+bool=True
+list=[1,2,3,4,5]
+tuple=(6,7,8,9,0)
+dict={"name":"Afshan","age":22,"degree":"BSCS"}
+print(int)
+print(type(int))
+print(str)
+print(type(str))
+print(float)
+print(type(float))
+print(bool)
+print(type(bool))
+print(list)
+print(type(list))
+print(tuple)
+print(type(tuple))
+print(dict)
+print(type(dict))
+
+#Task 2
+#OPERATORS
+#part 1
+#Write a Python script using two variables and perform the following operations Addition, Subtraction, Multiplication, Division, Modolo
+a=10
+b=5
+add=a+b
+sub=a-b
+mul=a*b
+div=a/b
+mod=a%b
+print("Addintion: ",add)
+print("Subtraction: ",sub)
+print("Multiplication: ",mul)
+print("Division: ",div)
+print("Modulo: ",mod)
+
+#part 2
+#Using Two Numerical Variables and Write a Python Scripts to Compare them using Comparison Operators.
+a=2
+b=6
+print(a>b)
+print(a<b)
+print(a==b)
+print(a!=b)
+print(a>=b)
+print(a<=b)
+
+#part 3
+#Write a Python script that demonstrates the use of assignment operators.
+a=10
+b=5
+a+=b
+print(a)
+a-=b
+print(a)
+a*=b
+print(a)
+a/=b
+print(a)
+a%=b
+print(a)
+a**=b
+print(a)
+a//=b
+print(a)
+
+#part 4
+#Write a Python script to demonstrate the use of logical operators and boolean variables.
+a=True
+b=False
+print(a and b)
+print(a or b)
+print(not a)
+print(not b)
+
+
+
+
